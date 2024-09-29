8d5ec62dff14bb72064567a49b52109fc2f2f4a9
Suzanne Vandeweerdt <suzanne.vandeweerdt@student.ucll.be>
2024-09-29 03:30:57 +0200

    Up to wizard duel 2

--- 02-oo/01-classes/09-local-library/student.py
+++ 02-oo/01-classes/09-local-library/student.py
@@ -1,17 +1,23 @@
 class Book:
     def __init__(self, title, author):
-        pass
+        self.title = title
+        self.author = author
 
 
 class Library:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.books = []
 
     def add_book(self, book):
-        pass
+        self.books.append(book)
 
     def remove_book(self, book):
-        pass
+        self.books.remove(book)
 
     def search_books(self, search_string):
-        pass
+        books_result = []
+        for i_book in self.books:
+            if search_string in i_book.title or search_string in i_book.author:
+                books_result.append(i_book)
+        return books_result
